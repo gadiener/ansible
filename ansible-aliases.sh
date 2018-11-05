ce8ce34871eb59e9a3be77a91482f0484c00cc74
Gabriele Diener <gabriele.diener@caffeina.com>
2018-11-05 11:34:51 +0100

    Escaped var

--- ansible-aliases.sh
+++ ansible-aliases.sh
@@ -10,7 +10,7 @@ PLAYBOOK_ENV=" -v \$(pwd):/playbook/"
 EXTRA_ENV=()
 REMOVE=false
 FILE_NAME=~/.ansible-aliases
-FILE_INCLUDE="source $FILE_NAME"
+FILE_INCLUDE="source ${FILE_NAME}"
 COMMANDS=(
 	'ansible'
 	'ansible-playbook'
@@ -30,12 +30,12 @@ SHELL_CONFIG_FILES=(
 )
 
 function error_message {
-	echo "[!] $1, use '--help' flag for more information"
+	echo "[!] ${1}, use '--help' flag for more information"
 	exit 1
 }
 
 function message {
-	echo ">> $1"
+	echo ">> ${1}"
 }
 
 function help_message {
@@ -53,48 +53,42 @@ function help_message {
 	exit 2
 }
 
-function implode {
-	local IFS="$1"
-	shift
-	echo "$*"
-}
-
 while [[ $# -gt 0 ]];do
-	key="$1"
+	key="${1}"
 
-	case $key in
+	case ${key} in
 		-h|--help)
 			help_message
 		;;
 		-k|--key)
 			if [ -z ${2+x} ]; then
-				error_message "Missing $key value"
+				error_message "Missing ${key} value"
 			fi
-			SSH_KEY_ENV="-e \"SSH_KEY=\$(cat $2)\""
+			SSH_KEY_ENV="-e \"SSH_KEY=\$(cat ${2})\""
 			shift
 			shift
 		;;
 		-p|--playbook)
 			if [ -z ${2+x} ]; then
-				error_message "Missing $key value"
+				error_message "Missing ${key} value"
 			fi
-			PLAYBOOK_ENV="-v $2:/playbook/"
+			PLAYBOOK_ENV="-v ${2}:/playbook/"
 			shift
 			shift
 		;;
 		-t|--tag)
 			if [ -z ${2+x} ]; then
-				error_message "Missing $key value"
+				error_message "Missing ${key} value"
 			fi
-			DOCKER_TAG=$2
+			DOCKER_TAG=${2}
 			shift
 			shift
 		;;
 		-e|--env)
 			if [ -z ${2+x} ]; then
-				error_message "Missing $key value"
+				error_message "Missing ${key} value"
 			fi
-			EXTRA_ENV+=("-e \"$2\"")
+			EXTRA_ENV+=" -e \"${2}\""
 			shift
 			shift
 		;;
@@ -103,48 +97,47 @@ while [[ $# -gt 0 ]];do
 			shift
 		;;
 		*)
-			error_message "Unrecognized option $key"
+			error_message "Unrecognized option ${key}"
     	;;
 	esac
 done
 
 for _FILE in ${SHELL_CONFIG_FILES[@]}; do
-	if [ -w "$_FILE" ]; then
-		if [ "$REMOVE" = true ]; then
-			if grep -q "$FILE_INCLUDE" $_FILE; then
-				sed -i.old "s#$FILE_INCLUDE##" $_FILE
-				message "Aliases file uninstalled from $_FILE"
+	if [ -w "${_FILE}" ]; then
+		if [ "${REMOVE}" = true ]; then
+			if grep -q "${FILE_INCLUDE}" ${_FILE}; then
+				sed -i.old "s#${FILE_INCLUDE}##" ${_FILE}
+				message "Aliases file uninstalled from ${_FILE}"
 			fi
 		else
-			if grep -q "$FILE_INCLUDE" $_FILE; then
-				message "Aliases file is already installed in $_FILE"
+			if grep -q "${FILE_INCLUDE}" ${_FILE}; then
+				message "Aliases file is already installed in ${_FILE}"
 			else
-				cp $_FILE $_FILE.old
-				echo $FILE_INCLUDE >> $_FILE
-				message "Aliases file installed in $_FILE"
+				cp ${_FILE} ${_FILE}.old
+				echo ${FILE_INCLUDE} >> ${_FILE}
+				message "Aliases file installed in ${_FILE}"
 			fi
 			break
 		fi
 	fi
 done
 
-if [ "$REMOVE" = true ]; then
-	if [ -w "$FILE_NAME" ]; then
-		rm $FILE_NAME
+if [ "${REMOVE}" = true ]; then
+	if [ -w "${FILE_NAME}" ]; then
+		rm ${FILE_NAME}
 		message "Aliases file removed"
 	fi
 	exit 0
 fi
 
-if [ ! -w "$FILE_NAME" ]; then
-	touch $FILE_NAME
+if [ ! -w "${FILE_NAME}" ]; then
+	touch ${FILE_NAME}
 else
-	> $FILE_NAME
+	> ${FILE_NAME}
 fi
 
 for _COMMAND in ${COMMANDS[@]}; do
-	EXTRA_ENV=`implode ' ' $EXTRA_ENV`
-	echo "alias ${_COMMAND}='docker run ${SSH_KEY_ENV} ${PLAYBOOK_ENV} ${EXTRA_ENV} -it ${DOCKER_IMAGE}:${DOCKER_TAG} ${_COMMAND}'" >> $FILE_NAME
+	echo "alias ${_COMMAND}='docker run ${SSH_KEY_ENV} ${PLAYBOOK_ENV} ${EXTRA_ENV} -it ${DOCKER_IMAGE}:${DOCKER_TAG} ${_COMMAND}'" >> ${FILE_NAME}
 	message "Alias ${_COMMAND} installed"
 done
 
